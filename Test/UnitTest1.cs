f1724cebaa14078d5a55362d999a62846e726759
Macan96 <66360946+Macan96@users.noreply.github.com>
2020-06-03 12:25:29 +0200

    First commit

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,14 +1,36 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace Test
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        IWebDriver driver;
+
+        [TestInitialize]
+        public void Setup()
         {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com/en");
+            driver.Manage().Window.Maximize();
+        }
+
+        [TestMethod]
+        public void ChromeSearch()
+        {
+            driver.FindElement(By.Name("q")).SendKeys("Mitto CH");
+            driver.FindElement(By.Name("q")).SendKeys(Keys.Enter);
+            var element = driver.FindElement(By.ClassName("TbwUpd NJjxre"));
+            Assert.IsTrue(element.Displayed);
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            driver.Quit();
         }
     }
 }
